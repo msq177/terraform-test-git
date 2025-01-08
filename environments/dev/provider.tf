60f59a6c9ace0872727716a42b0ef56dc2020d05
maoshuai.17 <maoshuai.17@bytedance.com>
2025-01-09 00:18:33 +0800

    feat: test pr

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     volcengine = {
-      source = "volcengine/volcengine"
+      source  = "volcengine/volcengine"
       version = "=0.0.156"
     }
   }
 
   backend "s3" {
-    bucket   = "terraform-demo" #存储桶名称
-    key      = "terraform-demo/terraform.tfstate" #对象名称
-    region   = "cn-beijing" #存储桶所在地域
+    bucket = "terraform-demo"                   #存储桶名称
+    key    = "terraform-demo/terraform.tfstate" #对象名称
+    region = "cn-beijing"                       #存储桶所在地域
     endpoints = {
       s3 = "https://tos-s3-cn-beijing.volces.com" #TOS的S3域名
     }
@@ -23,7 +23,7 @@ terraform {
 }
 
 provider "volcengine" {
-  region     = var.region
+  region = var.region
 }
 
 variable "region" {
--- environments/prod/cicd/main.tf
+++
@@ -1,8 +0,0 @@
-module "network" {
-  source = "../../../modules/network"
-
-  vpc_name     = "simple-vpc-dev"
-  vpc_cidr     = "10.4.0.0/16"
-  subnet_cidr = "10.4.0.0/24"
-  subnet_name  = "simple-subnet-dev"
-}
